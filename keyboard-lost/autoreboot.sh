2de56507e8643b2eb68b40339eef347d51eeb643
Bin Li <binli@gnome.org>
2023-06-01 00:04:50 +0800

    Finished script and run successfully

--- keyboard-lost/autoreboot.sh
+++ keyboard-lost/autoreboot.sh
@@ -6,13 +6,11 @@
 # Usage:
 #  ./autoreboot.sh 30
 
-# check if the argument is a number
-# if not, exit
-if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+# check if the argument is empty
+if [ -z "$1" ]; then
     echo "Usage: $0 <number>"
     exit 1
 fi
-
 # check if the argument is greater than 0
 # if not, exit
 if [ "$1" -le 0 ]; then
@@ -20,23 +18,56 @@ if [ "$1" -le 0 ]; then
     exit 1
 fi
 
-# Let Linux do autoreboot
+TIMES=$1
+USER=$(whoami)
+# copy this script into the home directory
+if [ ! -f "/home/$USER/autoreboot.sh" ]; then
+    cp $0 /home/$USER/autoreboot.sh
+fi
+
+# check if the autostart directory exists
+if [ ! -d "/home/$USER/.config/autostart" ]; then
+    mkdir -p /home/$USER/.config/autostart
+fi
+
+# check if the autostart file exists
+# if not, create it
+if [ ! -f "/home/$USER/.config/autostart/autoreboot.desktop" ]; then
+    cat <<EOF | tee /home/$USER/.config/autostart/autoreboot.desktop > /dev/null
+[Desktop Entry]
+Type=Application
+Exec=/usr/bin/gnome-terminal --maximize -- /bin/bash -c "cd /home/$USER ; ./autoreboot.sh $TIMES ; exec bash"
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=autoreboot
+Comment=autoreboot
+EOF
+fi
+
+# decrease the reboot times by 1
+# if the reboot times is 0, remove the autostart file
+# and exit
+# if the reboot times is not 0, reboot the system
 # and check journalctl's result if there is 'Elan TrackPoint' in the output
 # if there is, exit
 # if not, continue
-# repeat the above steps for 30 times
-# if there is no 'Elan TrackPoint' in the output for 30 times, exit
-# if there is 'Elan TrackPoint' in the output for 30 times, reboot
-for (( i=1; i<=$1; i++ ))
-do
-    echo "Rebooting... $i"
-    sudo reboot
-    sleep 60
-    if journalctl | grep -q 'Elan TrackPoint'; then
-        echo "Elan TrackPoint is still there"
-    else
-        echo "Elan TrackPoint is gone"
-        exit 1
-    fi
-done
+# after reboot, the autostart file will be executed again
+# and the reboot times will be decreased by 1
+if [ "$TIMES" -eq 0 ]; then
+    rm -f /home/$USER/.config/autostart/autoreboot.desktop
+    exit 0
+fi
 
+sed -i "s/autoreboot.sh $TIMES/autoreboot.sh $(( $TIMES - 1 ))/g" /home/$USER/.config/autostart/autoreboot.desktop
+
+if journalctl | grep -q 'Elan TrackPoint'; then
+    echo "Elan TrackPoint is still there"
+else
+    echo "Elan TrackPoint is gone"
+    exit 1
+fi
+echo "Rebooting... $TIMES"
+sleep 10
+reboot
+# End of autoreboot.sh
