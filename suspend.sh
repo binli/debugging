3d61ebf1883a974cba5517fead26162393b3262c
Bin Li <binli@ubuntu.com>
2025-04-27 17:03:22 +0800

    Support backend for suspend

--- suspend.sh
+++ suspend.sh
@@ -2,12 +2,20 @@
 
 # Number of suspends to perform
 TOTAL_SUSPENDS=${1:-30}
+BACKEND=$2
 
 # Loop to suspend 30 times
 for ((i=1; i<=TOTAL_SUSPENDS; i++)); do
     echo "Suspend #$i of $TOTAL_SUSPENDS starting..."
-    sudo rtcwake -m mem -s 10
+    sudo rtcwake -m mem -s 20
     echo "Woke up from suspend #$i"
+    if [ -x "$BACKEND" ]; then
+        echo "Call backend script: $BACKEND"
+        # get the backend return value
+        # if the return value is not 0, exit for debugging
+        # if the return value is 0, reboot the system
+        /home/$USER/$BACKEND
+    fi
     sleep 2  # Brief pause to avoid overwhelming the system
 done
 
